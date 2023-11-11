6b8f61a4380bc97ffd02aaf791d12df3aa76ad81
RORY ARYA PERMANA <88544095+R08888@users.noreply.github.com>
2023-11-12 03:57:32 +0700

    Add files via upload

---
+++ Rx2Client/Rx2Client.ino
@@ -0,0 +1,98 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+RH_RF95 rf95;
+int led = 13;
+unsigned long int millisBefore = 0;
+
+int turn = 1;
+int SNR1dump = 0, SNR2dump = 0;
+
+String dataTotal;
+String dataFromClient1;
+String dataFromClient2;
+String dataFromClient3;
+String RSSIclient1, RSSIclient2;
+
+void sendRequest(String request) {
+  String dataTotal = request;
+  int dataLength = dataTotal.length();
+  dataLength++;
+  uint8_t total[dataLength];
+  dataTotal.toCharArray(total, dataLength);
+  rf95.send(total, dataLength);
+  // Serial.print(",");
+  // Serial.println(rf95.lastRssi(), DEC);
+  rf95.waitPacketSent();
+}
+
+void waitForAnsware() {
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  if (rf95.waitAvailableTimeout(500)) {
+    if (rf95.recv(buf, &len)) {
+      // Serial.print("Received at Server: ");
+      String receivedData = (char*)buf;
+      // Serial.println(receivedData);
+
+      if (receivedData.startsWith("sensor1")) {
+        // Data dari Client 1
+        dataFromClient1 = receivedData;  // Hilangkan identifikasi klien (2 karakter pertama)
+        RSSIclient1 = rf95.lastRssi();
+        turn = 2;
+        // Serial.println("Data from Client 1: " + dataFromClient1);
+      } else if (receivedData.startsWith("sensor7")) {
+        // Data dari Client 2
+        dataFromClient2 = receivedData;
+        RSSIclient2 = rf95.lastRssi();
+        turn = 1;
+        // Serial.println("Data from Client 2: " + dataFromClient2);
+      }
+      // if (receivedData.startsWith("C3")) {
+      //   // Data dari Client 3
+      //   dataFromClient3 = receivedData.substring(2);
+      //   Serial.println("Data from Client 3: " + dataFromClient3);
+      // }
+    } else {
+      Serial.println("recv failed");
+    }
+  }
+}
+
+void data() {
+
+  Serial.println( dataFromClient1 + ",sensor5:" + RSSIclient1 + ",sensor6:" + SNR1dump + "," + dataFromClient2 + ",sensor11:" + RSSIclient2 + ",sensor12:" + SNR2dump + ",DATAEND");
+  // Serial.println("Data from Client 1: " + dataFromClient1 + "RSSI" + RSSIclient1);
+  // Serial.println("Data from Client 2: " + dataFromClient2 + "RSSI" + RSSIclient2);
+  SNR1dump++, SNR2dump++;
+  delay(20000);
+}
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial)
+    ;  // Wait for serial port to be available
+  if (!rf95.init())
+    Serial.println("init failed");
+  rf95.setFrequency(915.0);
+}
+
+void loop() {
+
+  switch (turn) {
+    case 1:
+      // Serial.println("Send Request to Client 1");
+      sendRequest("C1");  // Kirim permintaan ke Client 1
+      waitForAnsware();
+      delay(500);
+
+    case 2:
+      // Serial.println("Send Request to Client 2");
+      sendRequest("C2");
+      waitForAnsware();
+      // turn = 1;
+      delay(500);
+  }
+  data();
+}
---
+++ Testthingspeak4.ino
@@ -0,0 +1,177 @@
+#include <WiFi.h>
+#include "secrets.h"
+#include "ThingSpeak.h"
+
+#define serial_komunikasi Serial1
+
+char ssid[] = "TexasKost";
+char pass[] = "wisnuudahkerja";
+int keyIndex = 0;
+WiFiClient client;
+
+unsigned long myChannelNumber1 = 2258170;
+const char* myWriteAPIKey1 = "XL21UA8DTMVUXJLR";
+
+unsigned long myChannelNumber2 = 2336127;
+const char* myWriteAPIKey2 = "TKAHD6KL66OGR8NQ";
+
+String myStatus = "";
+
+String value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11, value12, data;
+// , value13, value14, value15, value16, value17, value18;
+int turn = 1;
+
+void parseData(String data) {
+  int sensor1StartIndex = data.indexOf("sensor1:") + 8;
+  int sensor1EndIndex = data.indexOf(",sensor2");
+  value1 = data.substring(sensor1StartIndex, sensor1EndIndex);
+
+  // if (sensor1StartIndex >= 0 && sensor1EndIndex > sensor1StartIndex) {
+  //   value1 = data.substring(sensor1StartIndex, sensor1EndIndex);
+  // }
+
+  int sensor2StartIndex = data.indexOf("sensor2:") + 8;
+  int sensor2EndIndex = data.indexOf(",sensor3");
+  value2 = data.substring(sensor2StartIndex, sensor2EndIndex);
+
+  int sensor3StartIndex = data.indexOf("sensor3:") + 8;
+  int sensor3EndIndex = data.indexOf(",sensor4");
+  value3 = data.substring(sensor3StartIndex, sensor3EndIndex);
+
+  int sensor4StartIndex = data.indexOf("sensor4:") + 8;
+  int sensor4EndIndex = data.indexOf(",sensor5");
+  value4 = data.substring(sensor4StartIndex, sensor4EndIndex);
+
+  int sensor5StartIndex = data.indexOf("sensor5:") + 8;
+  int sensor5EndIndex = data.indexOf(",sensor6");
+  value5 = data.substring(sensor5StartIndex, sensor5EndIndex);
+
+  int sensor6StartIndex = data.indexOf("sensor6:") + 8;
+  int sensor6EndIndex = data.indexOf(",sensor7");
+  value6 = data.substring(sensor6StartIndex, sensor6EndIndex);
+  //=====================================================================
+  int sensor7StartIndex = data.indexOf("sensor7:") + 8;
+  int sensor7EndIndex = data.indexOf(",sensor8");
+  value7 = data.substring(sensor7StartIndex, sensor7EndIndex);
+
+  int sensor8StartIndex = data.indexOf("sensor8:") + 8;
+  int sensor8EndIndex = data.indexOf(",sensor9");
+  value8 = data.substring(sensor8StartIndex, sensor8EndIndex);
+
+  int sensor9StartIndex = data.indexOf("sensor9:") + 8;
+  int sensor9EndIndex = data.indexOf(",sensor10");
+  value9 = data.substring(sensor9StartIndex, sensor9EndIndex);
+
+  int sensor10StartIndex = data.indexOf("sensor10:") + 9;
+  int sensor10EndIndex = data.indexOf(",sensor11");
+  value10 = data.substring(sensor10StartIndex, sensor10EndIndex);
+
+  int sensor11StartIndex = data.indexOf("sensor11:") + 9;
+  int sensor11EndIndex = data.indexOf(",sensor12");
+  value11 = data.substring(sensor11StartIndex, sensor11EndIndex);
+
+  int sensor12StartIndex = data.indexOf("sensor12:") + 9;
+  int sensor12EndIndex = data.indexOf(",DATAEND");
+  value12 = data.substring(sensor12StartIndex, sensor12EndIndex);
+}
+
+void setup() {
+  Serial.begin(115200);
+  serial_komunikasi.begin(9600, SERIAL_8N1, 27, 26);
+
+  while (!Serial) {
+    ;
+  }
+
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+  data = Serial1.readString();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass);
+      Serial.print(".");
+      delay(5000);
+    }
+
+    Serial.println("\nConnected.");
+  }
+
+
+  if (data.length() > 0) {
+    parseData(data);
+    // Serial.print(data);
+
+    if (turn == 1) {
+      ThingSpeak.setField(1, value1);
+      ThingSpeak.setField(2, value2);
+      ThingSpeak.setField(3, value3);
+      ThingSpeak.setField(4, value4);
+      ThingSpeak.setField(5, value5);
+      ThingSpeak.setField(6, value6);
+
+      int x1 = ThingSpeak.writeFields(myChannelNumber1, myWriteAPIKey1);
+
+      if (x1 == 200) {
+        Serial.println("Channel 1 update successful.");
+      } else if (x1 != 200) {
+        Serial.println("Problem updating channel. HTTP error code " + String(x1));
+      }
+      turn = 2;
+      delay(1000);
+    }
+    if (turn == 2) {
+
+      ThingSpeak.setField(1, value7);
+      ThingSpeak.setField(2, value8);
+      ThingSpeak.setField(3, value9);
+      ThingSpeak.setField(4, value10);
+      ThingSpeak.setField(5, value11);
+      ThingSpeak.setField(6, value12);
+
+      int x2 = ThingSpeak.writeFields(myChannelNumber2, myWriteAPIKey2);
+
+      if (x2 == 200) {
+        Serial.println("Channel 2 update successful.");
+      } else if (x2 != 200) {
+        Serial.println("Problem updating channel. HTTP error code " + String(x2));
+      }
+      turn = 1;
+            delay(1000);
+    }
+    // ThingSpeak.setStatus(myStatus);
+
+    Serial.print("sensor1:");
+    Serial.println(value1);
+    Serial.print("sensor2:");
+    Serial.println(value2);
+    Serial.print("sensor3:");
+    Serial.println(value3);
+    Serial.print("sensor4:");
+    Serial.println(value4);
+    Serial.print("sensor5:");
+    Serial.println(value5);
+    Serial.print("sensor6:");
+    Serial.println(value6);
+
+    Serial.print("sensor7:");
+    Serial.println(value7);
+    Serial.print("sensor8:");
+    Serial.println(value8);
+    Serial.print("sensor9:");
+    Serial.println(value9);
+    Serial.print("sensor10:");
+    Serial.println(value10);
+    Serial.print("sensor11:");
+    Serial.println(value11);
+    Serial.print("sensor12:");
+    Serial.println(value12);
+  }
+  delay(20000);
+}
